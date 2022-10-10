0cf8398add7833594b0032ed2c30e8b0fbb0813d
Balram Kumar <balramkgg99@gmail.com>
2022-10-10 15:50:16 +0530

    add increment one

--- my-app/src/State.js
+++ my-app/src/State.js
@@ -3,10 +3,17 @@
 import React, { useState } from "react";
 
 function State() {
-  let [data, setData] = useState("Balram");
+    // this below line is change the Balram into poddar using state
+//   let [data, setData] = useState("Balram");
+
+
+// the below line is for increase the count on virtual DOM 
+  let [data, setData] = useState(0);
 
   function updateData() {
-    setData("Poddar");
+    // setData("Poddar");
+    setData(data+1);
+
   }
   return (
     <div>
